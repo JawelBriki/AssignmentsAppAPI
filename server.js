0df3c692f6177e055a13ec3418fb2b036b4781b1
Jawel Briki <jawel.briki@etu.univ-cotedazur.fr>
2025-05-31 03:13:33 +0200

    update cors support

--- server.js
+++ server.js
@@ -32,11 +32,6 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-
-  if (req.method === 'OPTIONS') {
-      console.log("Responding to OPTIONS request");
-      return res.sendStatus(200);
-  }
   next();
 });
 
